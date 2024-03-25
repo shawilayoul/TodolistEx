22d624b995ee56130f68a4ceb64bd6e483762e2e
shawil ayoul <aocholayoul9@gmail.com>
2024-03-25 12:00:26 +0100

    working on delmete

--- index.js
+++ index.js
@@ -15,33 +15,23 @@ form.addEventListener("submit", (e) => {
     </div>
     `
     list.innerHTML += template
-    task.value = '';
+    localStorage.setItem('task',JSON.stringify(todos))
+    task = '';
   }else{
     alert('please enter a valid task')
   }
-  /*lists.push(task);
-  lists.forEach((item)=>{
-   const template = `
-   <div class="flex gap-2">
-   <div>${item}</div>
-   <button class="delete bg-red-400 text-white p-1 rounded">Delete</button>
-   <button class="update bg-blue-400 text-white p-1 rounded">Update</button>
-   </div>
-   `
-   list.innerHTML += template
-document.querySelector('.delete').addEventListener('click',()=>{
-    list.innerText = ""
-})
-document.querySelector('.update').addEventListener('click',()=>{
-    console.log('updated')
-})
-  })*/
   document.querySelector("#task") = ""
 });
 
-document.querySelector('.delete').addEventListener('click',()=>{
-    console.log()
+list.addEventListener('click',(e)=>{
+ if(e.target.classList.contains('delete')){
+    e.target.parentNode.remove();
+ }
 })
-document.querySelector('.update').addEventListener('click',()=>{
-    console.log('updated')
-})
+
+list.addEventListener('click',(e)=>{
+    task = document.querySelector("#task").value.trim();
+    if(e.target.classList.contains('update')){
+       task =   e.target.parentNode;
+    }
+   })
